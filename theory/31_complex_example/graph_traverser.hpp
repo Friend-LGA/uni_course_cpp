da3f129afbf81802f4b967ab22d9873dcb502a02
Grigorii Lutkov <friend.lga@gmail.com>
2021-12-07 18:24:08 +0300

    Change (const VertexId& id) -> (VertexId id)

--- theory/31_complex_example/graph_traverser.hpp
+++ theory/31_complex_example/graph_traverser.hpp
@@ -15,21 +15,21 @@ class GraphTraverser {
 
   explicit GraphTraverser(const Graph& graph) : graph_(graph) {}
 
-  void traverse(const Graph::VertexId& from_vertex_id,
+  void traverse(Graph::VertexId from_vertex_id,
                 const VertexCallback& vertex_callback,
                 const EdgeCallback& edge_callback) const;
 
-  void traverse_bfs(const Graph::VertexId& from_vertex_id,
+  void traverse_bfs(Graph::VertexId from_vertex_id,
                     const VertexCallback& vertex_callback,
                     const EdgeCallback& edge_callback) const;
 
-  void traverse_dfs(const Graph::VertexId& from_vertex_id,
+  void traverse_dfs(Graph::VertexId from_vertex_id,
                     const VertexCallback& vertex_callback,
                     const EdgeCallback& edge_callback) const;
 
   std::optional<std::vector<Graph::Vertex>> path_dijkstra(
-      const Graph::VertexId& from_vertex_id,
-      const Graph::VertexId& to_vertex_id) const;
+      Graph::VertexId from_vertex_id,
+      Graph::VertexId to_vertex_id) const;
 
  private:
   const Graph& graph_;
